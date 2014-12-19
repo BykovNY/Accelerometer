1f526ebc8f619f822c755fe99d236079e8652749
nikita <nbykov@osslabs.ru>
2014-12-19 16:30:13 +0400

    develop

--- app/src/main/java/com/bykovny/accelerometer/MainActivity.java
+++ app/src/main/java/com/bykovny/accelerometer/MainActivity.java
@@ -1,17 +1,51 @@
 package com.bykovny.accelerometer;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+
+    TextView mainValueTv;
+
+    SensorManager sensorManager;
+    Sensor accelerationSens;
+    Sensor linearAccelerationSens;
+
+    float[] accelValues = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainValueTv = (TextView) findViewById(R.id.main_value_tv);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        accelerationSens = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        linearAccelerationSens = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        sensorManager.registerListener(listener, accelerationSens, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        sensorManager.unregisterListener(listener);
     }
 
 
@@ -36,4 +70,28 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    void refreshTvValue(){
+        mainValueTv.setText(accelValues[0] + "m/s");
+    }
+
+    SensorEventListener listener = new SensorEventListener(){
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()){
+                case Sensor.TYPE_ACCELEROMETER:
+                    for (int i = 0; i < 3; i++){
+                        accelValues[i] = event.values[i];
+                        refreshTvValue();
+                    }
+                    break;
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 }
